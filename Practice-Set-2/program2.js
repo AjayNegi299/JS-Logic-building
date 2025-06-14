d5906e7f1c58bd3889de9fdf02bfcb4dcf6a2b8d
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-06-14 12:52:33 +0530

    new code

---
+++ Practice-Set-2/program2.js
@@ -0,0 +1,32 @@
+// write a function that determind smallest number from three numbers
+
+/*
+1. create function and take 3 input a, b,c.
+2. check a is smaller then b if yes then check a is smaller then d.
+3. return a result to functio
+4. call and print
+
+*/
+
+function smallestNumber(a,b,c){
+   let smaller = a;
+   let lebel = 'A'
+
+   if(b < smaller){
+    smaller = b
+    lebel = 'B'
+   }
+   if(c < smaller){
+    smaller = c
+    lebel = 'C'
+   }
+
+   return `${lebel} is smaller ${smaller}`
+}
+
+console.log(smallestNumber(10,12,14));
+console.log(smallestNumber(20,12,14));
+console.log(smallestNumber(16,15,14));
+console.log(smallestNumber(-10,12,14));
+console.log(smallestNumber(8,0,14));
+
