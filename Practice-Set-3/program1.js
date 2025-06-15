ad00acbde4f593730566fc9cf5a9ed378f9d7caf
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-06-15 11:28:32 +0530

    new code

--- Practice-Set-3/program1.js
+++ Practice-Set-3/program1.js
@@ -3,23 +3,39 @@
 /*
 1. create a fucntion that contain one para
 2. use loop for irriate input value
-3. 
+3. create empty var to hold multiple value
 */
 
-function factorial(val){
-     let fact = 1
-     for (let i = 1; i <= val; i++) {
-        fact = fact * i
-        
-     }
-     console.log(fact);
-
+function factorial(val) {
+  if (val < 0) {
+    throw new Error("Number should be positive");
+  }
+  let fact = 1;
+  for (let i = 1; i <= val; i++) {
+    fact = fact * i;
+  }
+  console.log(fact);
 }
-factorial(6)
-factorial(5)
-factorial(-5)
-factorial(0)
+factorial(6);
+factorial(5);
+factorial(-5);
+factorial(0);
 
+// Make this using recursion
 
+function calculateFactorialUsingRecursion(val) {
+  if (val < 0) {
+    throw new Error("Please! Enter a number less then zero and equal to zero");
+  }
 
-
+  if (val === 0 || val === 1) {
+    return 1;
+  }
+  return val * calculateFactorialUsingRecursion(val - 1);
+}
+console.log(calculateFactorialUsingRecursion(5));
+console.log(calculateFactorialUsingRecursion(0)); // 1
+console.log(calculateFactorialUsingRecursion(1)); // 1
+console.log(calculateFactorialUsingRecursion(3)); // 6
+console.log(calculateFactorialUsingRecursion(5)); // 120
+console.log(calculateFactorialUsingRecursion(-2)); // Throws Error
