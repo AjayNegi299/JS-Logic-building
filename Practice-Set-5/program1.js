ad48fe2789f24802e3ee4feb466027fab391ad74
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-06-21 12:26:20 +0530

    Practice set

---
+++ Practice-Set-5/program1.js
@@ -0,0 +1,15 @@
+function findPowerOfNumber(Base,Exponent){
+    if(Base <= 0 || Exponent <= 0){
+        throw new Error('Base and Exponent should be postive')
+    }
+    let result=1
+    for (let i = 1; i <= Exponent; i++) {
+        result *= Base        
+    }
+    return result
+}
+
+console.log(findPowerOfNumber(2,5));
+console.log(findPowerOfNumber(4,3));
+console.log(findPowerOfNumber(8,3));
+// console.log(findPowerOfNumber(15,0));
