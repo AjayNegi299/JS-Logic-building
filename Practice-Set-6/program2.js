0901ac29340dab0c4b0fe0e3918a7ec013688742
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-06-22 17:41:06 +0530

    update code

--- Practice-Set-6/program2.js
+++ Practice-Set-6/program2.js
@@ -1,6 +1,14 @@
 // write a function to calculate simple interest
 
 const calculateSimpleInterest=(principle,intRate,TimeDur)=>{
+    if(
+        typeof principle !== 'number' || isNaN(principle) || principle < 0
+        || typeof intRate !== 'number' || isNaN(principle) || principle < 0
+        || typeof TimeDur !== 'number' || isNaN(principle) || principle < 0
+
+    ){
+        throw new TypeError('Invalid input: Please enter positive numeric values only.')
+    }
     let interestAmount = (principle * intRate * TimeDur) / 100
     interestAmount = interestAmount.toFixed(2)
     return interestAmount
@@ -8,4 +16,4 @@ const calculateSimpleInterest=(principle,intRate,TimeDur)=>{
 console.log(calculateSimpleInterest(10000,10,5));
 console.log(calculateSimpleInterest(10000.75,10,5));
 console.log(calculateSimpleInterest(10000,10.2,2.5));
-console.log(calculateSimpleInterest(10000,10,5));
+console.log(calculateSimpleInterest(10000,10, 5));
