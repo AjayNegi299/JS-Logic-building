fb8d1af727287a1d5186aa6c2e7cabe97af7c1e4
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-06-26 16:08:12 +0530

    new Code

--- Practice-Set-8/program2.js
+++ Practice-Set-8/program2.js
@@ -1,1 +1,20 @@
-// write a function that make number pyramid
+// write a function that count the occurence of string
+const findOccurence = function(input){
+    if(typeof input === 'number'){
+        throw new TypeError("Input should be string only")
+    }
+    const obj = {}
+    const inputString = input.toLowerCase()
+    for (let i = 0; i < inputString.length; i++) {
+        if(!obj[inputString[i]]){
+            obj[inputString[i]] = 0
+        }
+        obj[inputString[i]] = obj[inputString[i]] +1   
+    }
+    return obj
+}
+
+console.log(findOccurence('Ajay Negi'));
+console.log(findOccurence('Nandan singh negi'));
+console.log(findOccurence('Hello world$$'));
+// console.log(findOccurence(77785214));
